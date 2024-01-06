f1dab65dd5b939906feaaf626b009ab8c3fdd3ad
Lucas <chromium@wynntils.com>
2024-01-06 16:35:38 +0100

    chore: make window max width larger

--- src/components/ui/Window.tsx
+++ src/components/ui/Window.tsx
@@ -9,7 +9,7 @@ type WindowProps = React.ComponentProps<"div"> & {
 export const Window = ({ title, children, className }: WindowProps) => {
     return (
       <div className={className}>
-        <section className="h-full w-full bg-zinc-900/25 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-zinc-900/95 flex flex-col gap-4 max-w-2xl">
+        <section className="h-full w-full bg-zinc-900/25 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-zinc-900/95 flex flex-col gap-4 max-w-4xl">
           <div className="w-full bg-zinc-950/95 flex items-center justify-between py-2 px-4 rounded-t-md">
             <div className="flex items-center space-x-2">
               <div className="w-3 h-3 rounded-full bg-red-500"></div>
