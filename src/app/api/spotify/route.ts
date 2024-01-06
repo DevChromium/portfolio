903fde3081fa732bc403f8a452c92e276e29e372
Lucas <chromium@wynntils.com>
2024-01-06 16:18:55 +0100

    fix: force route to be dynamic

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -1,6 +1,8 @@
 import { getNowPlaying } from "@/lib/spotify";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: NextRequest) {
 
     const response = await getNowPlaying()
