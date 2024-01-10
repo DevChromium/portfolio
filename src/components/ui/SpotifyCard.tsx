4425535ff2b21e19d4dda075e8d21e9333110435
Lucas <chromium@wynntils.com>
2024-01-10 22:05:06 +0100

    chore: make spotify card more beautiful

--- src/components/ui/SpotifyCard.tsx
+++ src/components/ui/SpotifyCard.tsx
@@ -3,7 +3,9 @@ import { msToTime } from "@/lib/converters";
 import { SpotifyResponse } from "@/types/Spotify";
 import Image from "next/image";
 import { ProgressBar } from "./ProgressBar";
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
+import { AnimatedText } from "../core/AnimatedText";
+import Marquee from "react-fast-marquee";
 
 interface SpotifyCardProps {
   data: SpotifyResponse;
@@ -11,6 +13,7 @@ interface SpotifyCardProps {
 }
 
 export const SpotifyCard = ({ data, onSongFinish }: SpotifyCardProps) => {
+  const spotifyCardRef = useRef<RefObject<HTMLDivElement>>();
   const [songData, setSongData] = useState(data);
   const [songProgress, setSongProgress] = useState(songData.progress);
   const [isFinished, setIsFinished] = useState(
@@ -40,63 +43,72 @@ export const SpotifyCard = ({ data, onSongFinish }: SpotifyCardProps) => {
   }, [isFinished, songData.duration, onSongFinish]);
 
   return (
-    <section className="bg-zinc-900/65 border border-zinc-900/95 rounded-md p-4 flex flex-col sm:flex-row gap-4 sm:items-center shadow-inner relative">
-      <Image
-        src={"/spotify.svg"}
-        alt="Spotify logo"
-        width={20}
-        height={20}
-        className="absolute top-3 right-3"
-      />
+    <section className="bg-zinc-900/65 border border-zinc-900/95 rounded-md p-4 shadow-inner space-y-4">
+      <div className="flex gap-2">
+        <Image src={"/spotify.svg"} alt="Spotify logo" width={20} height={20} />
+        <p className="max-w-prose text-sm font-semibold">
+          Listening to spotify
+        </p>
+      </div>
 
-      <Image
-        src={albumCover}
-        alt={`Cover of album: ${songData.album.name}`}
-        width={100}
-        height={100}
-        className="rounded-md shadow-sm"
-      />
-      <div className="w-full space-y-4">
-        <section>
-          <span className="text-md font-bold inline-flex items-center gap-2">
-            {songData.explicit ? <span>&#127348;</span> : <></>}
-            <a
-              className={`hover:underline`}
-              href={songData.url}
-              target="_blank"
-            >
-              {songData.name}
-            </a>
-          </span>
-
-          <p className="text-sm">
-            by{" "}
-            {artists.map((artist, index) => (
-              <a href={artist.url} key={index} target="_blank">
-                {index ? ", " : ""}
-                <span className="hover:underline">{artist.name}</span>
+      <div className="flex flex-col lg:flex-row gap-4">
+        <Image
+          src={albumCover}
+          alt={`Cover of album: ${songData.album.name}`}
+          width={100}
+          height={100}
+          className="rounded-md shadow-sm"
+        />
+        <div className="w-full">
+          <section>
+            <div className="text-md font-bold inline-flex items-center gap-2">
+              {songData.explicit ? (
+                <span className="bg-white text-black rounded-sm text-[8px] w-3 h-3 text-center">
+                  E
+                </span>
+              ) : (
+                <></>
+              )}
+              <a
+                className={`text-ellipsis hover:underline`}
+                href={songData.url}
+                target="_blank"
+              >
+                <span className="max-w-prose text-ellipsis">
+                  {songData.name}
+                </span>
               </a>
-            ))}
-          </p>
-          <p className="text-sm">
-            on{" "}
-            <a
-              href={songData.album.url}
-              target="_blank"
-              className="hover:underline"
-            >
-              {songData.album.name}
-            </a>
-          </p>
-        </section>
-        <div className="flex gap-2 items-center">
-          <p className="text-sm">{msToTime(songProgress)}</p>
-          <ProgressBar
-            value={(songProgress / songData.duration) * 100}
-            className="bg-green-400"
-          />
-          <p className="text-sm">{msToTime(songData.duration)}</p>
+            </div>
+
+            <p className="text-sm">
+              by{" "}
+              {artists.map((artist, index) => (
+                <a href={artist.url} key={index} target="_blank">
+                  {index ? ", " : ""}
+                  <span className="hover:underline">{artist.name}</span>
+                </a>
+              ))}
+            </p>
+            <p className="text-sm">
+              on{" "}
+              <a
+                href={songData.album.url}
+                target="_blank"
+                className="hover:underline"
+              >
+                {songData.album.name}
+              </a>
+            </p>
+          </section>
         </div>
+      </div>
+      <div className="flex gap-2 items-center">
+        <p className="text-sm">{msToTime(songProgress)}</p>
+        <ProgressBar
+          value={(songProgress / songData.duration) * 100}
+          className="bg-[#1DB954]"
+        />
+        <p className="text-sm">{msToTime(songData.duration)}</p>
       </div>
     </section>
   );
